fbe7b9f252ed28ff6afa13a043df2ad7002cbaf7
Kristijan <kristijan.zivcec@gmail.com>
2025-09-15 14:24:23 +0200

    Version 1.1.3 - 2025-09-15 14:24:23

--- src/components/tip-jar.ts
+++ src/components/tip-jar.ts
@@ -293,7 +293,7 @@ export class ICPayTipJar extends LitElement {
   }
 
   private startOnramp() {
-    try { window.dispatchEvent(new CustomEvent('icpay-sdk-method-start', { detail: { name: 'sendFundsUsd', type: 'onramp' } })); } catch {}
+    try { window.dispatchEvent(new CustomEvent('icpay-sdk-method-start', { detail: { name: 'createPaymentUsd', type: 'onramp' } })); } catch {}
     this.showWalletModal = false;
     setTimeout(() => this.createOnrampIntent(), 0);
   }
